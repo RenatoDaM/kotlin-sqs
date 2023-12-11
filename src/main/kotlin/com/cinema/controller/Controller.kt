13290662f0e1b3a05b263d04e383b597c9d932ae
Renato Davoli <renaato.d.m@gmail.com>
2023-12-11 17:44:44 -0300

    updating

--- src/main/kotlin/com/cinema/controller/Controller.kt
+++ src/main/kotlin/com/cinema/controller/Controller.kt
@@ -1,7 +1,6 @@
 package com.cinema.controller
 
 import com.cinema.model.Ticket
-import com.cinema.sqs.MessageReceiver
 import com.cinema.sqs.TicketProducer
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.MutableHttpResponse
@@ -13,24 +12,11 @@ import io.micronaut.http.annotation.Status
 import io.micronaut.serde.ObjectMapper
 
 @Controller
-class Controller (private val ticketProducer: TicketProducer,
-    private val messageReceiver: MessageReceiver) {
+class Controller (private val ticketProducer: TicketProducer) {
     @Post("/demo")
     @Status(HttpStatus.NO_CONTENT)
     fun publishDemoMessages(@Body ticket: Ticket) {
         val objectMapper: ObjectMapper = ObjectMapper.getDefault()
         ticketProducer.send(objectMapper.writeValueAsString(ticket))
-    }
-
-    @Get("/teste")
-    @Status(HttpStatus.NO_CONTENT)
-    suspend fun list() {
-        messageReceiver.sendMessages("https://sqs.us-east-1.amazonaws.com/123080688348/ticket_queue", "ooooakodsksao")
-    }
-
-    @Get("/receive")
-    @Status(HttpStatus.OK)
-    suspend fun receiveMessage(): MutableHttpResponse<String>? {
-        return messageReceiver.receiveMessages("https://sqs.us-east-1.amazonaws.com/123080688348/ticket_queue")
     }
 }
